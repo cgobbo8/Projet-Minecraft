2efcd4090895398d4fba0d45b523eb0430e85428
cgobbo <corentin.gobbo@hotmail.frf>
2019-02-27 11:45:19 +0100

    Add lib fx

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,7 +2,10 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
 
@@ -10,9 +13,21 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+
+			
+			Group root = new Group();
+			Scene scene = new Scene(root,1000,600);
+
+			int sizeL = (int) scene.getWidth();
+			int sizeH = (int) scene.getHeight();
+
+			Label la = new Label("Projet Minecraft");
+
+			la.setLayoutX(sizeL /2 - la.getWidth() - 20);
+			root.getChildren().addAll(la);
+
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
